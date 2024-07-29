357304683e8ec5e635afe7e5a410fd07fd5b37bb
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2024-07-30 03:57:56 +0530

    Add project files.

---
+++ Expense Tracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //category Id
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; }=DateTime.Now;
+    }
+}
