7e3753cbb4b450af80301d7804361104a7971939
pasindu dissanayake <123033890+maduradevDotNet@users.noreply.github.com>
2024-08-05 15:14:13 +0530

    delete button dev in index

--- Expense Tracker/Controllers/CategoryController.cs
+++ Expense Tracker/Controllers/CategoryController.cs
@@ -25,24 +25,6 @@ namespace Expense_Tracker.Controllers
             return View(await _context.Categories.ToListAsync());
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
 
         // GET: Category/AddOrEdit
         public IActionResult AddOrEdit(int id = 0)
@@ -83,24 +65,6 @@ namespace Expense_Tracker.Controllers
 
 
 
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -116,9 +80,7 @@ namespace Expense_Tracker.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
-        {
-            return _context.Categories.Any(e => e.CategoryId == id);
-        }
+     
     }
 }
+
